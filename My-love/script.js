7c1901af757d6a4dd4eb3d97012cb075d7849881
Wachirawit-Kunjai <tonkla001234@gmail.com>
2026-08-03 22:05:08 +0700

    upadte js

---
+++ My-love/script.js
@@ -0,0 +1,8 @@
+const yesBtn = document.getElementById("yes-btn");
+const noBtn = document.getElementById("no-btn");
+const question = document.getElementById("question");
+const gif = document.getElementById("gif");
+const btnGroup = document.querySelector(".btn-group");
+
+const page1 = document.getElementById("page1");
+
