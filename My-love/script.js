df549cd447d744bf8823b6984d1c1011b4bd73e3
Wachirawit-Kunjai <tonkla001234@gmail.com>
2026-08-03 22:13:55 +0700

    add notexts

--- My-love/script.js
+++ My-love/script.js
@@ -6,3 +6,13 @@ const btnGroup = document.querySelector(".btn-group");
 
 const page1 = document.getElementById("page1");
 
+const noTexts = [
+    "Are you sure?",
+    "Think it over again",
+    "Think about it again",
+    "Don't do this 🥺",
+    "That's so mean!",
+    "I'm sulking at you, Fatty! 😠",
+    "I'm sulking, so don't touch me! 😤"
+];
+
